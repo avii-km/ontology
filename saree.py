3df520c1bff3da608c38451ad32934bc4ca6464f
Tech <ayush.upadhyay@creditas.in>
2025-01-04 01:03:32 +0530

    mi

---
+++ saree.py
@@ -0,0 +1,72 @@
+Saree= {
+    "Pattern": [
+        "Mirror Work",
+        "Colorblock",
+        "Abstract",
+        "Floral",
+        "Self Design",
+        "Printed",
+        "Solid/Plain",
+        "Ombre",
+        "Geometric",
+        "Pleated",
+        "Embellished/Sequined",
+        "Polka Dots",
+        "Paisley",
+        "Stripes",
+        "Animal Print",
+        "Woven",
+        "Embroidered",
+        "Textured",
+        "Checks",
+        "Tie & Dye"
+    ],  # imp
+    "Type of Work": [
+        "Mirror Work",
+        "Mukaish",
+        "Hand Embroidery",
+        "Gota",
+        "Hand Painted Job",
+        "Patch",
+        "Ikat",
+        "Zardozi",
+        "Resham Work",
+        "Floral",
+        "Leheriya",
+        "Shibori",
+        "Printed",
+        "Embellished & Sequined",
+        "Hand Block",
+        "Lace Work",
+        "Zari",
+        "Block Print",
+        "Beads and Stones",
+        "Embellished/Sequined",
+        "Kalamkari",
+        "Patola",
+        "Woven",
+        "Embroidered",
+        "Thread Work",
+        "Bandhani",
+        "Tassels",
+        "Chikankari"
+    ],  # name cng
+    "Occasion": [
+        "Casual",
+        "Wedding",
+        "Fusion",
+        "Everyday Essentials",
+        "Formal",
+        "Semi Formal",
+        "Festive",
+        "Party"
+    ],  # imp
+    "Padding": [
+        "Padded",
+        "Non-Padded"
+    ],
+    "Rise Style": [
+        "High Waist",
+        "Mid Waist"
+    ],
+},
