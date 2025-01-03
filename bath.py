74889e8dbefa10168fbbf44c0dba4c36fc67e299
Tech <ayush.upadhyay@creditas.in>
2025-01-04 00:47:29 +0530

    min upd

---
+++ bath.py
@@ -0,0 +1,335 @@
+Bathroom_Vanities = {
+    "Top Material": [
+        "Resin",
+        "Natural stone",
+        "Wood",
+        "Porcelain",
+        "Vitreous China",
+        "Engineered quartz composite",
+        "Glass",
+        "Engineered Stone",
+        "Concrete",
+        "Engineered stone composite",
+        "Granite",
+        "Quartz",
+        "Cultured Marble",
+        "Stone",
+        "Engineered Marble",
+        "Engineered Solid Surface",
+        "Ceramic",
+        "Acrylic",
+        "Marble",
+        "Solid Surface"
+    ],
+    "Faucet Included": [
+        "Faucet Not Included",
+        "Faucet Included"
+    ],
+    "Top Color Family": [
+        "Cream",
+        "Brown",
+        "Black",
+        "Beige",
+        "Gray",
+        "Green",
+        "White"
+    ],
+    "Top weight (lb.)": "fix",
+    "Vanity Top Thickness (in.)": [
+        "3.0",
+        "4.0",
+        "1.9",
+        "1.5",
+        ".80",
+        "1.56",
+        "0.79",
+        "0.6",
+        "1.38",
+        "1.5625",
+        "0.81",
+        "0.80",
+        "0.4",
+        ".75",
+        "4.72",
+        "0.00",
+        "1.18",
+        "3.9",
+        "1.90",
+        "0.70",
+        "2",
+        "1.10",
+        "1.00",
+        "2.54",
+        "3.94",
+        "0.8",
+        "1.1875",
+        "2.00",
+        "3.5",
+        "1.13",
+        "0.5",
+        "1.0",
+        "4.5",
+        ".78",
+        "0.8125",
+        "1.1",
+        "1.50",
+        "2.36",
+        "1.3",
+        "1",
+        "1.58",
+        "0.56",
+        "6",
+        "5.1",
+        "0.63",
+        "1.25",
+        "0.78",
+        "5",
+        ".71",
+        "1.6",
+        ".70",
+        "1.27",
+        "0.71",
+        "0.75",
+        "10.16",
+        ".63",
+        "0.813",
+        "0.7",
+        "3.12",
+        "10.5",
+        "2.5",
+        "6.75",
+        "1.91",
+        "1.4",
+        "3"
+    ],
+    "Hardware Finish Family": [
+        "Chrome",
+        "Brass",
+        "Bronze",
+        "Black",
+        "Stainless Steel",
+        "Gold",
+        "Hardware Not Included",
+        "Wood",
+        "Brown",
+        "Copper",
+        "Nickel",
+        "Gray",
+        "Pewter",
+        "White"
+    ],
+    "Style": [
+        "Minimalist,Modern",
+        "Farmhouse,Modern",
+        "Coastal,Transitional",
+        "Art Deco",
+        "Casual,Classic,Coastal,Contemporary,Cottage,Designer,Glam,Minimalist,Modern,Style,Traditional,Transitional",
+        "Rustic",
+        "Farmhouse",
+        "Classic",
+        "Modern,Transitional",
+        "Classic,Cottage,Transitional",
+        "Industrial,Modern",
+        "Art Deco,Modern",
+        "Art Deco,Farmhouse",
+        "Cottage",
+        "Classic,Modern,Transitional",
+        "Contemporary,Modern",
+        "Contemporary",
+        "Classic,Cottage",
+        "Industrial",
+        "Farmhouse,Rustic",
+        "Farmhouse,Traditional",
+        "Modern",
+        "Traditional,Transitional",
+        "Transitional",
+        "Coastal",
+        "Coastal,Modern,Transitional",
+        "Classic,Transitional",
+        "Traditional",
+        "Farmhouse,Transitional"
+    ],
+    "Vanity Top Edge Type": [
+        "Bullnose",
+        "Round Over",
+        "Half Bullnose",
+        "Flat",
+        "Double Bevel",
+        "Quarter Round",
+        "Straight",
+        "Bevel",
+        "Miter",
+        "Ogee"
+    ],
+    "Backsplash Included": [
+        "Backsplash Not Included",
+        "Backsplash Included"
+    ],
+    "Vanity Type": [
+        "Floating",
+        "Corner",
+        "Freestanding"
+    ],
+    "Faucet Hole Spacing (in.)": [
+        "Single Hole, No Spacing - Single Hole",
+        "No Predrilled Hole",
+        "Single Hole, 8 in. Widespread",
+        "8 in. Faucet Hole Spacing, 8 in. Widespread",
+        "Single Hole",
+        "8 in. Faucet Hole Spacing",
+        "4 in. Faucet Hole Spacing"
+    ],
+    "Assembly Required": [
+        "Yes",
+        "No"
+    ],
+    "Label": [
+        "Exclusive",
+        "Top Rated",
+        "New",
+        "Bath Event"
+    ],
+    "Features": [
+        "Mildew Resistant,Mold Resistant,Predrilled,Scratch Resistant,Stain Resistant",
+        "Predrilled,Stain Resistant",
+        "Mildew Resistant,Predrilled",
+        "Mildew Resistant,Mold Resistant,Predrilled,Prefinished,Scratch Resistant,Sink Overflow Drain,Stain Resistant",
+        "Predrilled,Sink Overflow Drain"
+    ],
+    "Sidesplash Included": [
+        "Sidesplash Included",
+        "Sidesplash Not Included"
+    ],
+    "Remote Access": [
+        "Remote Access",
+        "No Remote Access"
+    ],
+    "Vanity Drawer Door Features": [
+        "Adjustable Hinges,Soft Close Door Hinges",
+        "Adjustable Hinges,Soft Close Door Hinges,Soft Close Drawer Hinges"
+    ],
+    "Material": [
+        "Wood (100%)",
+        "Quartz",
+        "Wood (N/A%)",
+        "PVC (60%), Plywood (40%)",
+        "Plywood (N/A%)"
+    ],
+    "Color Family": [
+        "Black",
+        "Cream",
+        "White",
+        "Gray"
+    ],
+}
+
+inside_bath = {
+    'cabinet': {
+        "Cabinet Color": [
+            "Red",
+            "Silver",
+            "Unfinished",
+            "Brown",
+            "Black",
+            "Orange",
+            "Blue",
+            "Yellow",
+            "Gray",
+            "Green",
+            "White"
+        ],
+        "Number of Shelves Included in Cabinet": [
+            "2",
+            "0",
+            "1",
+            "6",
+            "4",
+            "9",
+            "3"
+        ],
+        "Cabinet Shade": [
+            "Light",
+            "Medium",
+            "Dark"
+        ],
+        "Cabinet Material": [
+            "Composite",
+            "Solid Wood",
+            "Wood",
+            "Particle Board",
+            "Plywood",
+            "Iron",
+            "Medium Density Fiberboard (MDF)",
+            "Steel"
+        ],
+        "Medicine Cabinet Included": [
+            "Medicine Cabinet Not Included",
+            "Medicine Cabinet Included"
+        ],
+        "Mirror Included": [
+            "Mirror Not Included",
+            "Mirror Included"
+        ]
+    },
+    'Basin': {
+        'ht', 'wid', 'dep', 'len'
+    },
+    'sink': {
+        "Sink Color": [
+            "Silver",
+            "Brown",
+            "Beige/Bisque",
+            "Black",
+            "Blue",
+            "Gray",
+            "Green",
+            "White"
+        ],
+        "Number of Sinks": [
+            "Double Sink",
+            "Single Sink"
+        ],
+        "Sink Type": [
+            "Vessel",
+            "Farmhouse/Apron front",
+            "Drop-in",
+            "Undermount",
+            "Integral"
+        ],
+        "Sink Material": [
+            "Composite",
+            "Plastic",
+            "Glass",
+            "Porcelain",
+            "Ceramic",
+            "Acrylic",
+            "Marble",
+            "Solid Surface",
+            "Vitreous China",
+            "Cast Iron",
+            "Cultured Marble"
+        ],
+        "Sink Location": [
+            "Left",
+            "Center",
+            "Right"
+        ],
+        "Sink Shape": [
+            "Round",
+            "Rectangular",
+            "Specialty",
+            "Square"
+        ],
+    },
+    'mirror': {
+        "Mirror Features": [
+            "No Mirror Features",
+            "Lighted",
+            "Fog Free Mirror,Lighted",
+            "Beveled Edge Mirror,Lighted",
+            "Beveled Edge Mirror"
+        ],
+        'dim': ['ht', 'wid', ]
+
+    },
+}
